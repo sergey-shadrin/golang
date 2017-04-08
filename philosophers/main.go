248ed457b547601d3f540424ffbef58670fdd33f
sergey-shadrin <cps.shadrin@gmail.com>
2017-04-08 11:30:37 +0300

    Реализовал модель обедающих философов, при которой происходит ситуация взаимной блокировки

--- philosophers/main.go
+++ philosophers/main.go
@@ -7,20 +7,19 @@ import (
 )
 
 const (
-	millisecondsToTakeFork = 100
-	millisecondsToReleaseFork = 100
-	maxMillisecondsToContemplate = 1e3
-	maxMillisecondsToEat = 1e3
+	millisecondsToTakeFork = 1000
+	millisecondsToReleaseFork = 1000
+	maxMillisecondsToContemplate = 1000
+	maxMillisecondsToEat = 1000
+	forkValue = 1
 )
 
-type Fork struct {
-	isBusy bool
-}
+type Fork chan int
 
 type Philosopher struct {
 	name string
-	leftFork *Fork
-	rightFork *Fork
+	leftFork Fork
+	rightFork Fork
 }
 
 func (phil *Philosopher) Contemplate() {
@@ -46,20 +45,17 @@ func (phil *Philosopher) doEat() {
 	fmt.Printf("%v has finished eating after %v milliseconds\n", phil.name, timeToEat)
 }
 
-func (phil *Philosopher) takeFork(fork *Fork, forkName string) {
+func (phil *Philosopher) takeFork(fork Fork, forkName string) {
 	fmt.Printf("%v is going to take %v fork\n", phil.name, forkName)
 	time.Sleep(millisecondsToTakeFork * time.Millisecond)
-	for fork.isBusy {
-		//wait for fork to release
-	}
-	fork.isBusy = true
+	<-fork
 	fmt.Printf("%v has taken %v fork\n", phil.name, forkName)
 }
 
-func (phil *Philosopher) releaseFork(fork *Fork, forkName string) {
+func (phil *Philosopher) releaseFork(fork Fork, forkName string) {
 	fmt.Printf("%v is going to release %v fork\n", phil.name, forkName)
 	time.Sleep(millisecondsToReleaseFork * time.Millisecond)
-	fork.isBusy = false
+	fork <- forkValue
 	fmt.Printf("%v has released %v fork\n", phil.name, forkName)
 }
 
@@ -72,12 +68,17 @@ func (phil *Philosopher) HaveDinner() {
 
 func main() {
 	forks := [5]Fork{}
+	for i := 0; i < len(forks); i++ {
+		forks[i] = make(Fork, 1)
+		forks[i] <- forkValue
+	}
+
 	philosophers := [5]Philosopher{
-		{"Aristotle", &forks[0], &forks[1]},
-		{"Nietzsche", &forks[1], &forks[2]},
-		{"Kant", &forks[2], &forks[3]},
-		{"Freud", &forks[3], &forks[4]},
-		{"Plato", &forks[4], &forks[0]},
+		{"Aristotle", forks[0], forks[1]},
+		{"Nietzsche", forks[1], forks[2]},
+		{"Kant", forks[2], forks[3]},
+		{"Freud", forks[3], forks[4]},
+		{"Plato", forks[4], forks[0]},
 	}
 	for i := 0; i < len(philosophers); i++ {
 		go philosophers[i].HaveDinner()
