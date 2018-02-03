525e31a63d94f1ba9fd2f02f934065011c2dbc4b
sergey-shadrin <cps.shadrin@gmail.com>
2018-02-03 13:58:19 +0300

    Added .gitignore. Removed comment from main.go

--- gosite/main.go
+++ gosite/main.go
@@ -7,37 +7,6 @@ import (
 	"os"
 )
 
-/*
-func main() {
-	http.HandleFunc("/api/v1/list", func(writer http.ResponseWriter, _ *http.Request) {
-		response := `
-[
-  {
-    "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "name": "Black Retrospetive Woman",
-    "duration": 15,
-    "thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"
-  }
-]`
-		fmt.Fprint(writer, response)
-	})
-
-	http.HandleFunc("/api/v1/video/d290f1ee-6c54-4b01-90e6-d701748f0851", func(writer http.ResponseWriter, _ *http.Request) {
-		response := `
-{
-  "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-  "name": "Black Retrospetive Woman",
-  "duration": 15,
-  "thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
-  "url": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4"
-}
-`
-		fmt.Fprint(writer, response)
-	})
-	http.ListenAndServe(":8000", nil)
-}
-*/
-
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
 	file, err := os.OpenFile("/home/sergey/log/gosite.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
